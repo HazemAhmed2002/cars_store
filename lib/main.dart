ded52e8dfb3f73d809c624e660d2823627b7d06b
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-07-09 15:28:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:cars_store/view/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:ui' as ui;
+import 'package:device_preview/device_preview.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(DevicePreview(
+    builder: (context) => const MyApp(),
+  ));
+}
+
+double? screenWidth;
+double? screenHeight;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final windowSize = ui.window.physicalSize;
+    final screenScale = ui.window.devicePixelRatio;
+    screenWidth = windowSize.width / screenScale;
+    screenHeight = windowSize.height / screenScale;
+    return ScreenUtilInit(
+        designSize: Size(screenWidth!, screenHeight!),
+        builder: (context, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            builder: DevicePreview.appBuilder,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: LoginScreen(),
+          );
+        });
+  }
+}
